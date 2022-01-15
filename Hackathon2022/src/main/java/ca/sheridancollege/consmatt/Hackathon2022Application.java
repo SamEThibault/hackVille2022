0886fb17bcfbf0baf10c54c1201d439a9135ce23
Matthieu Constant <matthieu.constant7792@gmail.com>
2022-01-14 22:44:10 -0500

    First Commit
    This Commit includes the creation of the project. Added dependencies are in the POM.xml

---
+++ Hackathon2022/src/main/java/ca/sheridancollege/consmatt/Hackathon2022Application.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon2022Application.class, args);
+	}
+
+}
---
+++ Hackathon2022/src/test/java/ca/sheridancollege/consmatt/Hackathon2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.consmatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
